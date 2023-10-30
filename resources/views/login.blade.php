7ae5f7b4ee38eeac7d894c4582e8f5be73fbf54c
trirahaditya <rahaditya@mail.com>
2023-10-30 15:33:44 +0800

    edit view login and register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,14 +1,53 @@
-@if(session('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
-    </div>
-@endif
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .center-text {
+            text-align: center;
+        }
 
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <label for="nim">NIM:</label>
-    <input type="text" name="nim" id="nim" required>
-    <br>
-    <button type="submit">Login</button>
-</form>
-<p>Belum punya akun? <a href="{{ route('register') }}">Daftar disini</a></p>
+        .center-div {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body class="bg-light">
+    <div class="center-div">
+        <h1 class="center-text">Perhitungan Akar Kuadrat Bilangan</h1>
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">Login</div>
+                        <div class="card-body">
+                            @if(session('error'))
+                                <div class="alert alert-danger">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
+
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="nim" class="form-label">NIM:</label>
+                                    <input type="text" name="nim" id="nim" class="form-control" required>
+                                </div>
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                            <p class="mt-3">Belum punya akun? <a href="{{ route('register') }}">Daftar disini</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,36 +2,66 @@
 <html>
 <head>
     <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .center-text {
+            text-align: center;
+        }
+
+        .center-div {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+    </style>
 </head>
-<body>
-    <h1>Register</h1>
-
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
+<body class="bg-light">
+    <div class="center-div">
+        <h1 class="center-text">Perhitungan Akar Kuadrat Bilangan</h1>
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">Register</div>
+                        <div class="card-body">
+                            @if(session('success'))
+                                <div class="alert alert-success">
+                                    {{ session('success') }}
+                                </div>
+                            @endif
+    
+                            @if($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+    
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">Nama:</label>
+                                    <input type="text" name="name" id="name" class="form-control" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="nim" class="form-label">NIM:</label>
+                                    <input type="text" name="nim" id="nim" class="form-control" required>
+                                </div>
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-primary">Daftar</button>
+                                </div>
+                            </form>
+                            <p class="mt-3">Sudah punya akun? <a href="{{ route('login') }}">Login disini</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    @endif
-
-    @if($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-        <label for="name">Nama:</label>
-        <input type="text" name="name" id="name" required>
-        <br>
-        <label for="nim">NIM:</label>
-        <input type="text" name="nim" id="nim" required>
-        <br>
-        <button type="submit">Daftar</button>
-    </form>
-    <p>Sudah punya akun? <a href="{{ route('login') }}">Login disini</a></p>
+    </div>
 </body>
 </html>
